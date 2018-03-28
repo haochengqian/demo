9a7e9233ec7ee41e19fac631bcf60f9067632500
haochengqian11 <haochengqian11@gmail.com>
2018-03-28 21:25:26 +0800

    add write vector and word to pkl

--- Word2Vector/data/model/readModel.py
+++ Word2Vector/data/model/readModel.py
@@ -1,5 +1,19 @@
 from gensim.models import Word2Vec
+import pickle
+
+f = open('vocabulary','r')
+
+word = open('word.pkl','wb')
+vector = open('vector.pkl','wb')
+
+vocabulary = []
+
+for row in f:
+    p = row.split(' ')
+    vocabulary.append(p[0])
 
 model = Word2Vec.load('/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/model/word2vec_gensim')
 
-print model.most_similar("liuzilin")
+for wo in vocabulary:
+    pickle.dump(wo,word)
+    pickle.dump(model[wo],vector)
