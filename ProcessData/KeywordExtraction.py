738bb0f99bf5403a90c18a4f8eb0174eba68253c
haochengqian262 <123289710@qq.com>
2018-04-22 20:59:14 +0800

    fix bug: process db successfully

--- ProcessData/KeywordExtraction.py
+++ ProcessData/KeywordExtraction.py
@@ -13,19 +13,21 @@ cursor = db.cursor()
 # print("Keywords:", keywords)
 
 if __name__ == '__main__':
-    cursor.execute("select * from dbo_west_zone limit 1;")
+    cursor.execute("select * from dbo_west_zone limit 2,2;")
     rows = cursor.fetchall()
     stoppath = 'SmartStoplist.txt'
-    rake_object = rake.Rake(stoppath, 5, 3, 4)
+    rake_object = rake.Rake(stoppath)
     for row in rows:
-        sentenceList = rake.split_sentences(row[5])
-        stopwordpattern = rake.build_stop_word_regex(stoppath)
-        phraseList = rake.generate_candidate_keywords(sentenceList,stopwordpattern,stoppath)
-        wordscores = rake.calculate_word_scores(phraseList)
-        keywordcandidates = rake.generate_candidate_keyword_scores(phraseList,wordscores)
-        sortedKeywords = sorted(keywordcandidates.iteritems())
-        keywords = len(sortedKeywords)
-        for keyword in sortedKeywords[0:(keywords/3)]:
-            print "Keyword:", keyword[0], ", score:", keyword[1]
+        # sentenceList = rake.split_sentences(row[5])
+        # stopwordpattern = rake.build_stop_word_regex(stoppath)
+        # phraseList = rake.generate_candidate_keywords(sentenceList,stopwordpattern,stoppath)
+        # wordscores = rake.calculate_word_scores(phraseList)
+        # keywordcandidates = rake.generate_candidate_keyword_scores(phraseList,wordscores)
+        # sortedKeywords = sorted(keywordcandidates.iteritems(),reverse=True)
+        # keywords = len(sortedKeywords)
+        # for keyword in sortedKeywords[0:(keywords/3)]:
+        #     print "Keyword:", keyword[0], ", score:", keyword[1]
+        keywords = rake_object.run(row[5])
+        print keywords
     cursor.close()
     db.close()
