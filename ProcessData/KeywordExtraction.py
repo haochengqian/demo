76ba8adebf978d4bc8131c502ce0d3d5c605171c
haochengqian262 <123289710@qq.com>
2018-04-22 20:23:44 +0800

    add code to achieve keywordExtraction

--- ProcessData/KeywordExtraction.py
+++ ProcessData/KeywordExtraction.py
@@ -7,20 +7,25 @@ sys.setdefaultencoding('utf8')
 db = MySQLdb.connect("localhost","root","haochengqian262","artical")
 cursor = db.cursor()
 
-# stoppath = 'data/stoplists/SmartStoplist.txt'
-#
-# rake_object = rake.Rake(stoppath, 5, 3, 4)
-#
-# sample_file = open("data/docs/fao_test/w2167e.txt", 'r', encoding="iso-8859-1")
-# text = sample_file.read()
-#
+
 # keywords = rake_object.run(text)
 
-# 3. print results
-#print("Keywords:", keywords)
+# print("Keywords:", keywords)
 
 if __name__ == '__main__':
     cursor.execute("select * from dbo_west_zone limit 1;")
     rows = cursor.fetchall()
+    stoppath = 'SmartStoplist.txt'
+    rake_object = rake.Rake(stoppath, 5, 3, 4)
+    for row in rows:
+        sentenceList = rake.split_sentences(row[5])
+        stopwordpattern = rake.build_stop_word_regex(stoppath)
+        phraseList = rake.generate_candidate_keywords(sentenceList,stopwordpattern,stoppath)
+        wordscores = rake.calculate_word_scores(phraseList)
+        keywordcandidates = rake.generate_candidate_keyword_scores(phraseList,wordscores)
+        sortedKeywords = sorted(keywordcandidates.iteritems())
+        keywords = len(sortedKeywords)
+        for keyword in sortedKeywords[0:(keywords/3)]:
+            print "Keyword:", keyword[0], ", score:", keyword[1]
     cursor.close()
     db.close()
