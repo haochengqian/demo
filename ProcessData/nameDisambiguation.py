4b13459b264618a1403c0a07d8a1179da5405370
haochengqian262 <123289710@qq.com>
2018-05-05 14:50:23 +0800

    Add code: input db data to file.

--- ProcessData/nameDisambiguation.py
+++ ProcessData/nameDisambiguation.py
@@ -6,6 +6,19 @@ sys.setdefaultencoding('utf8')
 db = MySQLdb.connect("localhost","root","haochengqian262","name")
 cursor = db.cursor()
 
+f = open('/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/keyword', 'w')
+
 if __name__ == '__main__':
     cursor.execute("select * from keyword2")
-    
+    rows = cursor.fetchall()
+    for row in rows:
+        i = 0
+        for r in row:
+            i += 1
+            if i != 4:
+                r = r.replace("||"," ")
+            f.write(r + '\n')
+        f.write('\n')
+
+    cursor.close()
+    f.close()
