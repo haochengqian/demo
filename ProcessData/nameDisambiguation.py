6dc6b2a2a90d92a21c8ac51e3fccfde9e8097b04
haochengqian262 <123289710@qq.com>
2018-05-06 17:07:20 +0800

    Add code: compare two word.

--- ProcessData/nameDisambiguation.py
+++ ProcessData/nameDisambiguation.py
@@ -1,16 +1,21 @@
 import MySQLdb
 import sys
+import synonyms
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 db = MySQLdb.connect("localhost","root","haochengqian262","name")
+db2 = MySQLdb.connect("localhost", "root","haochengqian262", "afterTranslate")
 cursor = db.cursor()
+cursor2 = db2.cursor()
 
-f = open('/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/keyword', 'w')
+#f = open('/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/keyword', 'w')
 
 if __name__ == '__main__':
     cursor.execute("select * from keyword2")
     rows = cursor.fetchall()
+    cursor2.execute("s")
     for row in rows:
         i = 0
         for r in row:
