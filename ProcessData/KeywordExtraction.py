51fbee7e8a369a67773ac99f283ee6361e25e4a0
haochengqian262 <123289710@qq.com>
2018-07-13 15:03:02 +0800

    Add Code: Add word extraction.

--- ProcessData/KeywordExtraction.py
+++ ProcessData/KeywordExtraction.py
@@ -1,36 +1,16 @@
 from nlp_rake import rake
-import MySQLdb
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-db = MySQLdb.connect("localhost","root","haochengqian262","afterTranslate")
-cursor = db.cursor()
-
 
 # keywords = rake_object.run(text)
 
 # print("Keywords:", keywords)
 
-if __name__ == '__main__':
-    cursor.execute("select * from journal_all_1_5 limit 1;")
-    rows = cursor.fetchall()
-    stoppath = 'SmartStoplist.txt'
-    #rake_object = rake.Rake(stoppath)
-    for row in rows:
-        # sentenceList = rake.split_sentences(row[5])
-        # stopwordpattern = rake.build_stop_word_regex(stoppath)
-        # phraseList = rake.generate_candidate_keywords(sentenceList,stopwordpattern,stoppath)
-        # wordscores = rake.calculate_word_scores(phraseList)
-        # keywordcandidates = rake.generate_candidate_keyword_scores(phraseList,wordscores)
-        # sortedKeywords = sorted(keywordcandidates.iteritems(),reverse=True)
-        # keywords = len(sortedKeywords)
-        # for keyword in sortedKeywords[0:(keywords/3)]:
-        #     print "Keyword:", keyword[0], ", score:", keyword[1]
-        #keywords = rake_object.run(row[5])
-        for key in row:
-            key = key.replace('.','.\n')
-            key = key.replace(';',';\n')
-            print key
-    cursor.close()
-    db.close()
+def keywordExtraction(Abstract):
+    stopPath = 'SmartStoplist.py'
+    rake_object = rake.Rake(stopPath)
+    sentenceList = rake.split_sentences(Abstract)
+    stopWordPattern = rake.build_stop_word_regex(stopPath)
+    phraseList = rake.generate_candidate_keywords(sentenceList, stopWordPattern, stopPath)
+    wordScores = rake.calculate_word_scores(phraseList)
+    keywordCandidates = rake.generate_candidate_keyword_scores(phraseList, wordScores)
+    sortedKeywords = sorted(keywordCandidates.iteritems(), reverse=True)
+    return sortedKeywords
--- ProcessData/NameConvert/nametopinyin.py
+++ ProcessData/NameConvert/nametopinyin.py
@@ -4,19 +4,24 @@ import os
 import string
 
 mylist = []
-file_obj = open("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/name.txt")
+file_obj = open("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/familyname")
 all_lines = file_obj.readlines()
 for line in all_lines:
     line = line.strip('\r\n')
     mylist.append(lazy_pinyin(unicode(line,'utf-8')))
 file_obj.close()
 
-if os.path.exists("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTrans.txt"):
-    os.remove("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTrans.txt")
+if os.path.exists("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTransFamilyName.txt"):
+    os.remove("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTransFamilyName.txt")
 
-file_write_obj = open("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTrans.txt",'w')
+file_write_obj = open("/Users/mac/Documents/CODE/GraduateDesign/Word2Vector/data/nameAfterTransFamilyName.txt",'w')
+mylist2 = []
 for item in mylist:
-    item.pop()
+    if item not in mylist2:
+        mylist2.append(item)
+mylist2.sort()
+for item in mylist2:
+#    item.pop()
     file_write_obj.write(' '.join(item).encode("utf-8"))
     file_write_obj.write('\r\n')
 file_write_obj.close()
